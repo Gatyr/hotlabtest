7c9c2008b8f97ca80387a2d39de5fd60319d7823
Daniel Ertel-Moore <danielertelmoore@gmail.com>
2017-10-28 12:47:05 -0500

    enabled js

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,7 +1,7 @@
-// $(document).ready(function() {
-// 	$(".link-nav li").hover(function() {
-// 		$(this).css('background-color', 'black');
-// 	}, function() {
-// 		$(this).css('background-color', 'transparent');
-// 	});
-// });
+$(document).ready(function() {
+	$(".link-nav li").hover(function() {
+		$(this).css('background-color', 'black');
+	}, function() {
+		$(this).css('background-color', 'transparent');
+	});
+});
