c166fab5608ee64360d784bc526a991a8d633919
Daniel Ertel-Moore <danielertelmoore@gmail.com>
2017-10-28 12:00:16 -0500

    first push

---
+++ assets/javascript/home.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+// 	$(".link-nav li").hover(function() {
+// 		$(this).css('background-color', 'black');
+// 	}, function() {
+// 		$(this).css('background-color', 'transparent');
+// 	});
+// });
