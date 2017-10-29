abb9391c36fccb6c01813160b380eea2e330b34c
Daniel Ertel-Moore <danielertelmoore@gmail.com>
2017-10-28 20:46:13 -0500

    pushing css changes

--- assets/javascript/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".link-nav li").hover(function() {
-		$(this).css('background-color', 'black');
-	}, function() {
-		$(this).css('background-color', 'transparent');
-	});
-});
